7d62f3bef14075c2a8e20ee90196a2eba7352716
AuroraFreire <lourencofreire3@gmail.com>
2026-07-31 19:54:39 +0100

    add cooldown

--- scripts/hotbar.gd
+++ scripts/hotbar.gd
@@ -6,6 +6,8 @@ const SLOT_CLASS = preload("res://scripts/slot.gd")
 const STARTNG_ITEMS = ["StarterDrill", "Medkit"]
 
 var count = 0
+var active_item
+var cooldown_left = 0.0
 
 func _ready() -> void:
 	update_active_item_label()
@@ -18,6 +20,13 @@ func _ready() -> void:
 		PlayerInventory.active_item_updated.connect(slot.refresh_style)
 		slot.refresh_style()
 
+func _process(delta):
+	if cooldown_left > 0:
+		cooldown_left -= delta
+		$Cooldown.value = max(cooldown_left, 0)
+	if $Cooldown.value == 0:
+		$Cooldown.visible = false
+
 func update_active_item_label():
 	if slots[PlayerInventory.active_item_slot].item != null:
 		if slots[PlayerInventory.active_item_slot].item.item_name == "StarterDrill":
@@ -29,3 +38,12 @@ func update_active_item_label():
 		count += 1
 	else:
 		$ActiveItemLabel.text = ""
+
+func start_cooldown():
+	$Cooldown.visible = true
+	var active_item = slots[PlayerInventory.active_item_slot].item
+	if active_item == null:
+		return
+	cooldown_left = float(JsonData.item_data[active_item.item_name].get("Cooldown", 0))
+	$Cooldown.max_value = cooldown_left
+	$Cooldown.value = cooldown_left
--- scripts/planet_1.gd
+++ scripts/planet_1.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
 @onready var slots = $"../UserInterface/Hotbar/GridContainer".get_children()
+@onready var hotbar = $"../UserInterface/Hotbar"
 var item
 var quantity
+var active_item
 
 func _on_button1_pressed() -> void:
-	if slots[PlayerInventory.active_item_slot].item.item_name == "StarterDrill":
+	if hotbar.cooldown_left > 0:
+		return
+	active_item = slots[PlayerInventory.active_item_slot].item
+	if active_item != null and active_item.item_name == "StarterDrill":
 		quantity = randi_range(1, 4)
 		item = randi_range(1, 100)
 		if item == 1:
@@ -16,3 +21,5 @@ func _on_button1_pressed() -> void:
 			$"../UserInterface/Inventory".add_item("PureWater", quantity)
 		else:
 			$"../UserInterface/Inventory".add_item("AzureMoss", quantity)
+		hotbar.start_cooldown()
+	
