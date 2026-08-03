e7a894cad1e10af87c2dbfcb953b30acb6fc7ae3
LourencoFreire <lourencofreire3@gmail.com>
2026-08-03 21:36:37 +0100

    home banner to return to the lab

---
+++ scripts/home_banner.gd
@@ -0,0 +1,12 @@
+extends Node
+
+@onready var home_animation = $"../../HomeBannerAnim"
+
+func _on_texture_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/lab.tscn")
+
+func _on_texture_button_mouse_entered() -> void:
+	home_animation.play("home_banner_down")
+
+func _on_texture_button_mouse_exited() -> void:
+	home_animation.play("home_banner_up")
--- scripts/world1.gd
+++ scripts/world1.gd
@@ -1,8 +1,18 @@
 extends Node2D
 
+@onready var animation_home = $HomeBannerAnim
+
 func _input(event: InputEvent) -> void:
 	close_game(event)
 
 func close_game(event: InputEvent) -> void:
 	if event.is_action_pressed("close_game"):
 		get_tree().quit()
+
+
+func _on_home_banner_mouse_entered() -> void:
+	animation_home.play("home_banner_down")
+
+
+func _on_home_banner_mouse_exited() -> void:
+	animation_home.play("home_banner_up")
