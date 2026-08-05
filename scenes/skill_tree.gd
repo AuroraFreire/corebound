7e5039aeb35526ce3e10d7163ea5ae7d5cfc8c26
LourencoFreire <lourencofreire3@gmail.com>
2026-08-05 12:06:23 +0100

    pan around and zoom

---
+++ scenes/skill_tree.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@export var zoom_speed = 0.05
+@export var min_zoom = 0.1
+@export var max_zoom = 2.0
+@export var drag_speed = 1.5
+@onready var camera = $Camera2D
+var isPressed = false
+var e = Vector2.ZERO
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion and isPressed:
+		e = event.relative * drag_speed
+		camera.position -= e
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_MIDDLE:
+			isPressed = event.pressed
+		elif event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			camera.zoom = Vector2.ONE * min(max_zoom, camera.zoom.x + zoom_speed)
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			camera.zoom = Vector2.ONE * max(min_zoom, camera.zoom.x - zoom_speed)
--- skill_button.gd
+++ skill_button.gd
@@ -6,6 +6,5 @@ class_name SkillNode
 func _ready() -> void:
 	if get_parent() is SkillNode:
 		skill_branch.clear_points()
-		skill_branch.add_point(skill_branch.to_local(self.global_position + self.size * self.scale / 2))
-		skill_branch.add_point(skill_branch.to_local(get_parent().global_position + get_parent().size * get_parent().scale / 2))
-		
+		skill_branch.add_point(skill_branch.to_local(self.global_position + self.size / 0.67))
+		skill_branch.add_point(skill_branch.to_local(get_parent().global_position + get_parent().size / 0.67))
